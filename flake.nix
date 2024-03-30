b1d53bdb21fc2ea5da4558b047f8c5cd324aef63
r33drichards <rwendt1337@gmail.com>
2024-03-29 21:07:15 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flakery.url = "github:getflakery/flakes";
   };
 
-  outputs = { self, nixpkgs, flakery }: {
+  outputs = { self, nixpkgs }: {
     nixosConfigurations.hello-flakery = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        flakery.nixosModules.flakery
         ./configuration.nix
       ];
     };
