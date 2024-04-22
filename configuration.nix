c3137093716c4bbe71552fabdd25f5c09cbcc697
r33drichards <rwendt1337@gmail.com>
2024-04-22 01:15:38 -0700

    Revert "Update configuration.nix"
    This reverts commit 9d7c80602733b978be21cf01fe85a256e4ab48a6.

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,14 @@
 { config, pkgs, ... }:
-''''
+let 
+  flakeryDomain = builtins.readFile /metadata/flakery-domain;
+in
 {
   system.stateVersion = "23.05";
 
-    services.static-web-server = {
-      enable = true;
-      listen = "[::]:8888";
-      root = toString (pkgs.writeTextDir "nixos-test.html" ''
-        <h1>Hello NixOS!</h1>
-      '');
-      configuration = {
-        general = { directory-listing = true; };
-      };
-    };
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/tsauthkey";
+    extraUpFlags = [ "--ssh" ];
+  };
+
 }
