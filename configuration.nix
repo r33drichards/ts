0f4f23394b7a2f2c9dc038f08823517269f0fc80
r33drichards <rwendt1337@gmail.com>
2024-05-08 01:50:55 -0700

    set passwd

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }:
-let 
+let
   flakeryDomain = builtins.readFile /metadata/flakery-domain;
 in
 {
@@ -11,13 +11,14 @@ in
     extraUpFlags = [ "--ssh" ];
   };
 
-    users.users.flakery = {
+  users.users.flakery = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    password = "flakery"; # Set the password for the user.
   };
   # allow sudo without password for wheel
   security.sudo.wheelNeedsPassword = false;
-  
+
   services.openssh = {
     enable = true;
     # require public key authentication for better security
@@ -27,7 +28,7 @@ in
 
   users.users."flakery".openssh.authorizedKeys.keys = [
     # replace with your ssh key 
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/e5M8zDNH5DUmqCGKM0OhHKU5iHFum3IUekq8Fqvegur7G2fhsmQnp09Mjc5pEw2AbfTYz11WMHsvC5WQdRWSS2YyZHYsPb9zIsVBNcss+H5x63ItsDjmbrS6m/9r7mRBOiN265+Mszc5lchFtRFetpi9f+EBis9r8atyPlsz86IoS2UxSSWonBARU4uwy2+TT7+mYg3cQf7kp1Y1sTqshXmcHUC5UVSRk3Ny9IbIMhk19fOxr3y8gaXoT5lB0NSLO8XFNbNT6rjZXH1kpiPJh3xLlWBPQtbcLrpm8oSS51zH7+zAGb7mauDHu2RcfBgq6m1clZ6vff65oVuHOI7" 
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/e5M8zDNH5DUmqCGKM0OhHKU5iHFum3IUekq8Fqvegur7G2fhsmQnp09Mjc5pEw2AbfTYz11WMHsvC5WQdRWSS2YyZHYsPb9zIsVBNcss+H5x63ItsDjmbrS6m/9r7mRBOiN265+Mszc5lchFtRFetpi9f+EBis9r8atyPlsz86IoS2UxSSWonBARU4uwy2+TT7+mYg3cQf7kp1Y1sTqshXmcHUC5UVSRk3Ny9IbIMhk19fOxr3y8gaXoT5lB0NSLO8XFNbNT6rjZXH1kpiPJh3xLlWBPQtbcLrpm8oSS51zH7+zAGb7mauDHu2RcfBgq6m1clZ6vff65oVuHOI7"
   ];
 
 }
